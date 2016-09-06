dfbaf44bc2c0a3b1449c022d15119bc32d100282
Stefan Dzisiewski-Smith <stefan@bareconductive.com>
2016-09-06 16:56:49 +0100

    Initial public commit

---
+++ colour-spin.py
@@ -0,0 +1,78 @@
+################################################################################
+#
+# Bare Conductive Pi Cap
+# ----------------------
+#
+# colour-spin.py - rainbow colours on Pi Cap LED
+#
+# Written for Raspberry Pi.
+#
+# Bare Conductive code written by Szymon Kaliski.
+#
+# This work is licensed under a Creative Commons Attribution-ShareAlike 3.0
+# Unported License (CC BY-SA 3.0) http://creativecommons.org/licenses/by-sa/3.0/
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#################################################################################
+
+from time import sleep
+import signal, sys
+import RPi.GPIO as GPIO
+
+red_led_pin = 6
+green_led_pin = 5
+blue_led_pin = 26
+
+# init GPIO using BCM pinout
+# look here for more info on pins: http://pinout.xyz
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# set up color pins as outputs
+GPIO.setup(red_led_pin, GPIO.OUT)
+GPIO.setup(green_led_pin, GPIO.OUT)
+GPIO.setup(blue_led_pin, GPIO.OUT)
+
+def light_rgb(r, g, b):
+  # we are inverting the values, because the LED is active LOW
+  # LOW - on
+  # HIGH - off
+  GPIO.output(red_led_pin, not r)
+  GPIO.output(green_led_pin, not g)
+  GPIO.output(blue_led_pin, not b)
+
+# handle ctrl+c gracefully
+def signal_handler(signal, frame):
+  light_rgb(0, 0, 0)
+  GPIO.cleanup()
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# how long should each colour stay lit before switching to next one
+delay_time = 0.5
+
+while True:
+  light_rgb(0, 0, 1) # blue
+  sleep(delay_time)
+  light_rgb(0, 1, 0) # green
+  sleep(delay_time)
+  light_rgb(1, 0, 0) # red
+  sleep(delay_time)
+  light_rgb(0, 1, 1) # cyan
+  sleep(delay_time)
+  light_rgb(1, 1, 0) # yellow
+  sleep(delay_time)
+  light_rgb(1, 0, 1) # magenta
+  sleep(delay_time)
+  light_rgb(1, 1, 1) # white
+  sleep(delay_time)
+  light_rgb(0, 0, 0) # black (off)
+  sleep(delay_time)
