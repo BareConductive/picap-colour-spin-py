20ad77e706e21f2fe73709e0f11adef5cf3d9ea4
tomhartley <tomhartley@rocketmail.com>
2018-08-06 15:20:19 +0100

    Added Python 3 support and refactored code

--- colour-spin.py
+++ colour-spin.py
@@ -7,11 +7,11 @@
 #
 # Written for Raspberry Pi.
 #
-# Bare Conductive code written by Szymon Kaliski.
+# Bare Conductive code written by Szymon Kaliski and Tom Hartley
 #
 # This work is licensed under a MIT license https://opensource.org/licenses/MIT
 #
-# Copyright (c) 2016, Bare Conductive
+# Copyright (c) 2018, Bare Conductive
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -34,56 +34,28 @@
 #################################################################################
 
 from time import sleep
-import signal, sys
-import RPi.GPIO as GPIO
+from gpiozero import RGBLED
 
-red_led_pin = 6
-green_led_pin = 5
-blue_led_pin = 26
-
-# init GPIO using BCM pinout
-# look here for more info on pins: http://pinout.xyz
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-# set up color pins as outputs
-GPIO.setup(red_led_pin, GPIO.OUT)
-GPIO.setup(green_led_pin, GPIO.OUT)
-GPIO.setup(blue_led_pin, GPIO.OUT)
-
-def light_rgb(r, g, b):
-  # we are inverting the values, because the LED is active LOW
-  # LOW - on
-  # HIGH - off
-  GPIO.output(red_led_pin, not r)
-  GPIO.output(green_led_pin, not g)
-  GPIO.output(blue_led_pin, not b)
-
-# handle ctrl+c gracefully
-def signal_handler(signal, frame):
-  light_rgb(0, 0, 0)
-  GPIO.cleanup()
-  sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
+led = RGBLED(6, 5, 26, active_high=False)
 
 # how long should each colour stay lit before switching to next one
 delay_time = 0.5
 
-while True:
-  light_rgb(0, 0, 1) # blue
-  sleep(delay_time)
-  light_rgb(0, 1, 0) # green
-  sleep(delay_time)
-  light_rgb(1, 0, 0) # red
-  sleep(delay_time)
-  light_rgb(0, 1, 1) # cyan
-  sleep(delay_time)
-  light_rgb(1, 1, 0) # yellow
-  sleep(delay_time)
-  light_rgb(1, 0, 1) # magenta
-  sleep(delay_time)
-  light_rgb(1, 1, 1) # white
-  sleep(delay_time)
-  light_rgb(0, 0, 0) # black (off)
-  sleep(delay_time)
+ordering = [(0, 0, 1), #blue 
+            (0, 1, 0), #green
+            (1, 0, 0), #red
+            (0, 1, 1), #cyan
+            (1, 1, 0), #yellow
+            (1, 0, 1), #magenta
+            (1, 1, 1), #white
+            (0, 0, 0)] #black (off)
+
+running = True
+while running:
+  try:
+    for i in ordering:
+      led.color = i
+      sleep(delay_time)
+  except KeyboardInterrupt:
+    led.off()
+    running = False
